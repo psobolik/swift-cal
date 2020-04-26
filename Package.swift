b933959492ce2b2ab9498087227f9669aa79e459
Paul Sobolik <psobolik@zoho.com>
2020-04-26 14:16:09 -0400

    Only depends on SPMUtilitiy, not SwiftPM

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swift-cal",
-            dependencies: ["SwiftPM"]),
+            dependencies: ["SPMUtility"]),
         .testTarget(
             name: "swift-calTests",
             dependencies: ["swift-cal"]),
